585519e161d2920f626c28f4de8a6a22ddebdb0a
moto <moto@hellomoto.ai>
2019-10-11 23:13:12 -0700

    Get rid of FXML in MainView

--- MotorImageryProcessing/src/main/kotlin/ai/hellomoto/mip/MainView.kt
+++ MotorImageryProcessing/src/main/kotlin/ai/hellomoto/mip/MainView.kt
@@ -2,21 +2,23 @@ package ai.hellomoto.mip
 
 import ai.hellomoto.mip.settings.board_config.BoardConfigFragment
 import ai.hellomoto.mip.tasks.rotation_task.RotationTaskProcessorView
-import javafx.application.Platform
-import javafx.scene.control.*
-import javafx.scene.layout.AnchorPane
 import tornadofx.*
 
 class MainView : View() {
-    override val root: AnchorPane by fxml()
-    private val menuBar:MenuBar by fxid("menuBar")
-    private val mSettingsBoard:MenuItem by fxid("mSettingsBoard")
-    private val mTasksRotationRecord:MenuItem by fxid("mTasksRotationRecord")
-
-    init {
-        Platform.setImplicitExit(true)
-        menuBar.prefWidthProperty().bind(root.widthProperty())
-        mTasksRotationRecord.action { replaceWith<RotationTaskProcessorView>() }
-        mSettingsBoard.action { find<BoardConfigFragment>().openModal() }
+    override val root = vbox {
+        prefHeight = 400.0
+        prefWidth = 600.0
+        menubar {
+            menu("Settings") {
+                item("Board") {
+                    action { find<BoardConfigFragment>().openModal() }
+                }
+            }
+            menu("Tasks") {
+                item("Rotation") {
+                    action { replaceWith<RotationTaskProcessorView>() }
+                }
+            }
+        }
     }
 }
